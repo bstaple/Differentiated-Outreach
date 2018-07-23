8d9b19143c7024a6940389f08c7c21ce77e5cd7a
Brandon Staple <bstaple1999@gmail.com>
2018-07-23 16:23:24 -0400

    STUDENTS INCLUDED

--- Final.py
+++ Final.py
@@ -1,25 +1,47 @@
 import webapp2
 
-class Room(object):
-	def __init__(self, name, user):
+class Profiles(object):
+	def __init__(self, Student, Hosts):
 		self.name = name
-		self.host_user = user
+		self.notes = ''
+
+
+class Host(object):
+	def __init__(self, name, notes=''):
+		self.name = name
+		self.notes = ''
+
+class Student(object):
+	def __init__(self, name, notes='', Host):
+		self.name = name
+		self.notes = notes
+		self.Host = Host
+
+class Room(object):
+	def __init__(self, name, host, students):
+		self.name = name
+		self.host_user = host
+		self.student_list = students
 
 rooms = [
-	Room('Room 1', 'user1'),
-	Room('Room 2', 'user2'),
-	Room('Room 3', 'user1')
+	Room('Room 1', 'host1', 'Jimmy'),
+	Room('Room 2', 'host2', 'Carl'),
+	Room('Room 3', 'host3', 'Jack, Sally and John')
 ]
 
 class ShowRoomsHandler(webapp2.RequestHandler):
 	def dispatch(self):
 		for room in rooms:
-			self.response.out.write(room.name + ' is owned by ' + room.host_user)
+			self.response.out.write(room.name + ' is owned by ' + room.host_user + ' ' + room.student_list)
 			self.response.out.write('<br>')
+
 
 
 class CreateRoomHandler(webapp2.RequestHandler):
 	def dispatch(self):
+		# for Room in rooms:
+		# 	if Room not in rooms:
+		# 		rooms.append('Room 4', 'User 4')
 		self.response.out.write('This is working.')
 
 
